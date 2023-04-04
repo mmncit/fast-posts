3133722e9d779d16f6e1ef21ec65191be0d5b689
Mamun <mmncit@gmail.com>
2023-04-03 21:58:44 -0600

    add alembic resion for posts

---
+++ alembic/versions/cf72cfc834f2_create_post_table.py
@@ -0,0 +1,50 @@
+"""create post table
+
+Revision ID: cf72cfc834f2
+Revises:
+Create Date: 2023-04-03 21:46:59.532463
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'cf72cfc834f2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Create the posts table
+    op.create_table(
+        "posts",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("title", sa.String(300), nullable=False),
+        sa.Column("content", sa.Text, nullable=False),
+        sa.Column("published",
+                  sa.Boolean,
+                  nullable=False,
+                  server_default=sa.false()),
+        sa.Column("updated_at",
+                  sa.TIMESTAMP,
+                  nullable=False,
+                  server_default=sa.func.now(),
+                  onupdate=sa.func.now()),
+        sa.Column("created_at",
+                  sa.TIMESTAMP,
+                  nullable=False,
+                  server_default=sa.func.now()),
+        sa.Column("user_id",
+                  sa.Integer,
+                  sa.ForeignKey("users.id", ondelete="CASCADE"),
+                  nullable=False),
+    )
+
+    # Create the indexes
+    op.create_index("idx_posts_user_id", "posts", ["user_id"])
+
+
+def downgrade() -> None:
+    op.drop_table("posts")
