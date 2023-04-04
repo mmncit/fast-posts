0588a9359b9d8eecf745be48f91da577ca79629f
Mamun <mmncit@gmail.com>
2023-04-03 19:34:11 -0600

    resolve spelling mistake

--- api/orm/oauth2.py
+++ api/orm/oauth2.py
@@ -1,16 +1,18 @@
-from jose import JWTError, jwt
-from datetime import datetime, timedelta
-from . import schemas, database, models
-from fastapi import Depends, status, HTTPException
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
 import os
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from . import database, models, schemas
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 # SECRET_KEY
 # Algorithm
-# Expriation time
+# Expiration time
 
 SECRET_KEY = os.environ['SECRET_KEY']
 ALGORITHM = os.environ['ALGORITHM']
@@ -43,12 +45,15 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme),
-                     db: Session = Depends(database.get_db)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(database.get_db)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail=f"Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"})
+        headers={"WWW-Authenticate": "Bearer"}
+    )
     token = verify_access_token(token, credentials_exception)
     print(token)
     user = db.query(models.User).filter(models.User.id == token.id).first()
